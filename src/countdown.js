ba02b9d77b1a1910134de22cb17fa7f43ad5aa0a
prax78 <prax78@gmail.com>
2023-04-16 23:45:43 +0530

    Add files via upload

---
+++ src/countdown.js
@@ -0,0 +1,19 @@
+setInterval(getCountDown, 2000);
+
+
+function getCountDown() {
+    const one = Date.now();
+    const two = new Date(2023, 10, 10);
+    const result = two.getTime() - one;
+    const today = new Date(one);
+
+    const refhrs = 23 - today.getHours();
+    const refmnts = 59 - today.getMinutes();
+    const refsec = 59 - today.getSeconds();
+
+
+
+    document.getElementById("displaycount").innerHTML = Math.floor(result / 86400000) + " " + refhrs + " " + refmnts + " " + refsec;
+
+
+}
