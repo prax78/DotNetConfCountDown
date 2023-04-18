bf73cf5397b3e9431432c5df0cfc29613f2081f9
prax78 <prax78@gmail.com>
2023-04-19 01:43:43 +0530

    Add files via upload

--- countdown.js
+++ countdown.js
@@ -1,4 +1,5 @@
 setInterval(getCountDown, 2000);
+let colorlist=['#FF1414','#FF147A','#FF14C9','#FF14F0','#C114FF','#A214FF','#8214FF','#2414FF','#147AFF','#14E8FF','#14FFC9','#14FF72','#14FF2C','#5BFF14','#B9FF14','#FFE814'];
 
 
 function getCountDown() {
@@ -11,9 +12,31 @@ function getCountDown() {
     const refmnts = 59 - today.getMinutes();
     const refsec = 59 - today.getSeconds();
 
-
+  
 
     document.getElementById("displaycount").innerHTML = Math.floor(result / 86400000) + " " + refhrs + " " + refmnts + " " + refsec;
+    document.getElementById("note").innerHTML = "This is my personal countdown app to countdown";
+
+    document.getElementById("note1").innerHTML = "the number of approx. days left in 2023 for the annual DotNet event";
+    
+    document.getElementById("note2").innerHTML = new Date( Date.now()).toDateString();
+   
+
+  
+    colorlist.map(x=>document.getElementById("note2").style.color=x);
+    colorlist.pop();
+    
+
+    if(colorlist.length==0)
+    {
+        colorlist=['#FF1414','#FF147A','#FF14C9','#FF14F0','#C114FF','#A214FF','#8214FF','#2414FF','#147AFF','#14E8FF','#14FFC9','#14FF72','#14FF2C','#5BFF14','#B9FF14','#FFE814'];
+    }
 
 
+    
+    
+   
+   
+    
 }
+ 
